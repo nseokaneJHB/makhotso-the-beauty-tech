48dd3dcfc44e645fea9d1149ea65559a93e01444
nolan <nolanseokane@gmail.com>
2025-03-04 01:32:22 +0200

    Refactor: update the logo

--- components/server/navigation.tsx
+++ components/server/navigation.tsx
@@ -5,13 +5,12 @@ export const Navigation = () => {
   return (
     <header className="dark:border-muted dark:bg-background fixed inset-x-0 top-0 z-50 flex items-center justify-center border-b border-pink-100 bg-pink-100 shadow backdrop-blur-sm">
       <nav className="flex w-full max-w-7xl items-center justify-between p-4 sm:px-8">
-        <Link href="/" title="Home" className="flex flex-col">
-          <span className="font-serif text-2xl font-extrabold tracking-widest text-pink-500">
-            MK
-          </span>
-          <span className="text-muted-foreground tracking-wider italic">
-            The Beauty Tech
-          </span>
+        <Link
+          href="/"
+          title="Home"
+          className="font-serif text-2xl font-extrabold tracking-widest text-pink-500"
+        >
+          MK
         </Link>
         <ToggleTheme />
       </nav>
